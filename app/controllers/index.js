ef97acc588054d8844d9b230ac75d555e86d4a62
Jens Rud Nielsen <JRN@Jenss-MacBook-Pro.local>
2018-02-04 21:33:59 +0100

    Added firebase

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,27 +2,27 @@
 /*import { computed, observer } from '@ember/object';*/
 /*import { empty } from '@ember/object/computed';*/
 import { match, not } from '@ember/object/computed';
-
 import Controller from '@ember/controller';
 
 export default Controller.extend({
     
     headerMessage: 'Coming Soon',
-
+    responseMessage: '',
     emailAddress: '',
-    /*isDisabled: computed('emailAddress', function() {
-      return this.get('emailAddress') === '';
-    })*/
-    /*isDisabled: empty('emailAddress')*/
+
     isDisabled: not('isValid'),
     isValid: match('emailAddress', /^.+@.+\..+$/),
 
     actions: {
 
       saveInvitation() {
-        alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
-        this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-        this.set('emailAddress', '');
+        const email = this.get('emailAddress');
+        const newInvitation = this.store.createRecord('invitation', { email: email });
+        newInvitation.save().then(response => {
+          this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+          this.set('emailAddress', '');
+        });
+        
       }
     }
 });
--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,23 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+
+    firebase: {
+      apiKey: "AIzaSyBBjhkapveLeEhIy8vBPIDgRJ-4wHPExvI",
+      authDomain: "emberlibraryapp-96aa1.firebaseapp.com",
+      databaseURL: "https://emberlibraryapp-96aa1.firebaseio.com",
+      projectId: "emberlibraryapp-96aa1",
+      storageBucket: "",
+      messagingSenderId: "633054092782"
+    },
+
+    // if using ember-cli-content-security-policy
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
