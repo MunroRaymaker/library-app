3fc962230e4dfe36372751bcade045b7c8ceccbc
Jens Rud Nielsen <JRN@Jenss-MacBook-Pro.local>
2018-02-11 13:42:15 +0100

    Adds seeder page and author and book model

---
+++ app/components/number-box.js
@@ -0,0 +1,8 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+
+    /* CSS Class names */
+    classNames: ['panel', 'panel-warning']
+
+});
--- app/models/library.js
+++ app/models/library.js
@@ -1,10 +1,13 @@
-import DS from 'ember-data';
+
 import { notEmpty } from '@ember/object/computed';
+import { hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
   address: DS.attr('string'),
   phone: DS.attr('string'),
 
+  books: DS.hasMany('book'),
+  
   isValid: notEmpty('name')
 });
